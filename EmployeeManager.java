cd46b096d9e444b3718ab6ee59eea2ebd0bc69a3
naimur4321 <nishadnaim72@gmail.com>
2025-11-21 11:36:18 +0600

    error handeled and task9 completed

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class EmployeeManager {
 
+
     public static String[] readEmployees() throws Exception {
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
@@ -18,7 +19,6 @@ public class EmployeeManager {
         }
     }
 
-    
     public static void main(String[] args) {
 
         // Argument validation
@@ -28,7 +28,9 @@ public class EmployeeManager {
             return;
         }
 
-        if (args[0].equals("l")) {
+        String arg = args[0];
+
+        if (arg.equals("l")) {
             System.out.println("Loading data ...");
             try {
                 for (String employee : readEmployees()) {
@@ -38,7 +40,7 @@ public class EmployeeManager {
             System.out.println("Data Loaded.");
         }
 
-        else if (args[0].equals("s")) {
+        else if (arg.equals("s")) {
             System.out.println("Loading data ...");
             try {
                 String[] employees = readEmployees();
@@ -47,20 +49,19 @@ public class EmployeeManager {
             } catch (Exception e) {}
             System.out.println("Data Loaded.");
         }
-
-        else if (args[0].contains("+")) {
+        else if (arg.startsWith("+")) {
             System.out.println("Loading data ...");
             try {
-                String nameInput = args[0].substring(1);
+                String nameInput = arg.substring(1);
                 writeEmployees(String.join(",", readEmployees()) + ", " + nameInput);
             } catch (Exception e) {}
             System.out.println("Data Loaded.");
         }
 
-        else if (args[0].contains("?")) {
+        else if (arg.startsWith("?")) {
             System.out.println("Loading data ...");
             try {
-                String searchName = args[0].substring(1);
+                String searchName = arg.substring(1);
                 boolean found = Arrays.stream(readEmployees())
                         .map(String::trim)
                         .anyMatch(emp -> emp.equals(searchName));
@@ -72,45 +73,45 @@ public class EmployeeManager {
             System.out.println("Data Loaded.");
         }
 
-        else if (args[0].contains("c")) {
+        else if (arg.equals("c")) {
             System.out.println("Loading data ...");
             try {
                 String[] employees = readEmployees();
-
                 int wordCount = employees.length;
-
                 int charCount = String.join(",", employees).length();
-
                 System.out.println(wordCount + " word(s) | " + charCount + " characters");
             } catch (Exception e) {}
             System.out.println("Data Loaded.");
         }
 
-        else if (args[0].contains("u")) {
+        else if (arg.startsWith("u")) {
             System.out.println("Loading data ...");
             try {
                 String[] employees = readEmployees();
-                String nameInput = args[0].substring(1);
-
+                String nameInput = arg.substring(1);
                 for (int i = 0; i < employees.length; i++) {
                     if (employees[i].trim().equals(nameInput)) {
                         employees[i] = "Updated";
                     }
                 }
-
                 writeEmployees(String.join(",", employees));
             } catch (Exception e) {}
             System.out.println("Data Updated.");
         }
 
-        else if (args[0].contains("d")) {
+        else if (arg.startsWith("d")) {
             System.out.println("Loading data ...");
             try {
                 List<String> employeeList = new ArrayList<>(Arrays.asList(readEmployees()));
-                employeeList.remove(args[0].substring(1));
+                employeeList.remove(arg.substring(1));
                 writeEmployees(String.join(",", employeeList));
             } catch (Exception e) {}
             System.out.println("Data Deleted.");
+        }
+
+        else {
+            System.out.println("Invalid argument: \"" + arg + "\"");
+            System.out.println(Constants.USAGE_MESSAGE);
         }
     }
 }
